29aa4710c45a69a2d9d5b55fbe299fe3faa0f389
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-12-15 20:20:59 +0800

    Fixed navigation afrer login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import useAuthStore from "../store/useAuthStore";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, SetPassword] = useState("");
+  const { login } = useAuthStore();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(username, password, navigate);
+  };
+
   return (
-    <div
-      className="flex flex-col items-center
-     min-h-screen
-     py-10 font-poppins"
-    >
+    <div className="flex flex-col items-center min-h-screen py-10 font-poppins">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
         <h1 className="text-2xl text-center mb-6">Log in</h1>
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
           <div className="form-control">
             <label>Username</label>
             <input
               type="text"
               placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               className="input input-bordered w-full"
             />
           </div>
@@ -23,6 +33,8 @@ const Login = () => {
             <input
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => SetPassword(e.target.value)}
               className="input input-bordered w-full"
             />
           </div>
