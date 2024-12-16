b9bcef506dd7b1f87e85ddf8a8a02c38391221ac
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-12-16 11:26:15 +0800

    update and delete modified

--- frontend/src/product/ProductDetails.jsx
+++ frontend/src/product/ProductDetails.jsx
@@ -1,13 +1,61 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useProductStore } from "../store/useProductStore.";
+import Modal from "react-modal";
+import { toast } from "react-toastify";
 
 const ProductDetails = () => {
   const products = useProductStore((state) => state.products);
   const fetchProducts = useProductStore((state) => state.fetchProducts);
+  const updateProduct = useProductStore((state) => state.updateProduct);
+  const deleteProduct = useProductStore((state) => state.deleteProduct);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState(null);
 
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
+
+  const openUpdateModal = (product) => {
+    setCurrentProduct(product);
+    setIsModalOpen(true);
+  };
+
+  const closeUpdateModal = () => {
+    setIsModalOpen(false);
+    setCurrentProduct(null);
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const { success, message } = await updateProduct(
+      currentProduct._id,
+      currentProduct
+    );
+    if (success) {
+      toast.success(message);
+      closeUpdateModal();
+    } else {
+      toast.error(message);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const { success, message } = await deleteProduct(id);
+      if (!success) {
+        toast.success(message);
+      } else {
+        toast.error(message);
+      }
+    } catch (error) {
+      toast.error("Failed to delete, Try again");
+    }
+  };
+
+  const handleChange = (e) => {
+    setCurrentProduct({ ...currentProduct, [e.target.name]: e.target.value });
+  };
 
   return (
     <div className="mt-5 text-center">
@@ -34,8 +82,18 @@ const ProductDetails = () => {
                   <td>${product.price}</td>
                   <td>{product.category}</td>
                   <td className="">
-                    <button className="btn btn-info mr-3">Edit</button>
-                    <button className="btn btn-warning">Delete</button>
+                    <button
+                      onClick={() => openUpdateModal(product)}
+                      className="btn btn-info mr-3"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDelete(product._id)}
+                      className="btn btn-warning"
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               ))
@@ -49,6 +107,99 @@ const ProductDetails = () => {
           </tbody>
         </table>
       </div>
+
+      {/* update modal  */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeUpdateModal}
+        contentLabel="Update Product Modal"
+        className="right-modal"
+        overlayClassName="right-modal-overlay"
+      >
+        <h2 className="text-xl font-semibold mb-4">Update Product</h2>
+        {currentProduct && (
+          <form onSubmit={handleUpdate}>
+            <label className="block mb-2">
+              Product name:
+              <input
+                type="text"
+                name="name"
+                value={currentProduct.name}
+                onChange={handleChange}
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="block mb-2">
+              Quantity:
+              <input
+                type="number"
+                name="quantity"
+                value={currentProduct.quantity}
+                onChange={handleChange}
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="block mb-2">
+              Price:
+              <input
+                type="number"
+                name="price"
+                value={currentProduct.price}
+                onChange={handleChange}
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="block mb-2">
+              Category:
+              <select
+                name="category"
+                value={currentProduct.category}
+                onChange={handleChange}
+                className="select select-bordered w-full"
+              >
+                <option value="">Select Category</option>
+                <option value="Gadget">Gadget</option>
+                <option value="Apparel">Apparel</option>
+                <option value="Vehicle">Vehicle</option>
+                <option value="Appliances">Appliances</option>
+                <option value="Shoes">Shoes</option>
+              </select>
+            </label>
+            <label className="block mb-2">
+              Image URL:
+              <input
+                type="text"
+                name="image"
+                value={currentProduct.image}
+                onChange={handleChange}
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="block mb-2">
+              Description:
+              <textarea
+                type="text"
+                name="description"
+                value={currentProduct.description}
+                onChange={handleChange}
+                className="textarea textarea-bordered w-full"
+              />
+            </label>
+            <div className="flex justify-end mt-4">
+              <button type="submit" className="btn btn-info mr-5">
+                Update
+              </button>
+              <button
+                onClick={closeUpdateModal}
+                type="button"
+                className="btn btn-neutral"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        )}
+      </Modal>
     </div>
   );
 };
