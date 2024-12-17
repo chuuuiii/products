b88f929a6d1cedde15b478e86dcac5bc75e8a3ab
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-12-17 13:38:46 +0800

    Route modified

--- frontend/src/routes/AppRoute.jsx
+++ frontend/src/routes/AppRoute.jsx
@@ -7,15 +7,22 @@ import Signup from "../pages/Signup";
 import Login from "../pages/Login";
 import UserDashboard from "../dashboard/UserDashboard";
 import CreateProduct from "../product/CreateProduct";
+import ProductCategory from "../category/ProductCategory";
+import CategoryPage from "../category/CategoryPage";
 
 const AppRoute = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        <Route path="/" element={<ProductCategory />}>
+          <Route index element={<CategoryPage />} />
+          <Route path="category/:category" element={<CategoryPage />} />
+        </Route>
 
         <Route path="/user" element={<UserDashboard />}>
           <Route path="products" element={<CreateProduct />} />
